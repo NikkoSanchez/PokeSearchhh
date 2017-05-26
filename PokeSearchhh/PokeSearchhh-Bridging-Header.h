62e4d337870d53b40749fb99a362be6606de6ba3
Nikko Sanchez <neeks08@gmail.com>
2017-05-25 18:35:09 -0700

    First

---
+++ PokeSearchhh/PokeSearchhh-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeSearchhh-Bridging-Header.h
+//  PokeSearchhh
+//
+//  Created by Nikko on 5/25/17.
+//  Copyright © 2017 Nikko Sanchez. All rights reserved.
+//
+
+#import "GeoFire.h"
---
+++ PokeSearchhh/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PokeSearchhh
+//
+//  Created by Nikko on 5/25/17.
+//  Copyright © 2017 Nikko Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var geoFire: GeoFire!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
